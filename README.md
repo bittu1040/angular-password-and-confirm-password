a125f1a8407c26c0fc06dca459b2147ffdf1225a
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-01-31 17:42:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-password-and-confirm-password
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-reactive-forms-kdccvl)
---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
+}
